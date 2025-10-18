f807356d5a9dcf14568f26fb45be9d83a60c5f06
brandon <brandongoodman58@gmail.com>
2025-10-19 07:14:50 +0800

    lets go

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+        // S3 bucket for Playwright reports
+        const reportsBucket = new s3.Bucket(this, 'PlaywrightReportsBucket01', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            encryption: s3.BucketEncryption.S3_MANAGED,
+            enforceSSL: true,
+            removalPolicy: cdk.RemovalPolicy.RETAIN,
+            autoDeleteObjects: false,
+        });
+  }
+}
